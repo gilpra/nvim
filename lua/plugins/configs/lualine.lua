aed3216bfd28081735a63171dde6f13032410ab3
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-08 08:45:21 +0700

    feat: add lualine plugin with custom UI config

---
+++ lua/plugins/configs/lualine.lua
@@ -0,0 +1,54 @@
+return {
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+
+    disabled_filetypes = {
+      statusline = { "NvimTree", "starter" },
+      winbar = {},
+    },
+
+    globalstatus = true, -- more lightweight than per-window
+  },
+
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = {
+      { "branch",      icons_enabled = true },
+      { "diff",        colored = false },
+      { "diagnostics", sources = { "nvim_lsp" } },
+    },
+
+    lualine_c = {
+      {
+        "filename",
+        path = 1, -- relative path
+        symbols = { modified = " ", readonly = " " },
+      },
+    },
+
+    lualine_x = {
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+
+  inactive_sections = {
+    lualine_a = { "filename" },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+
+  tabline = {},
+  extensions = {},
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,12 @@
 return {
   { "tiagovla/tokyodark.nvim" },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+
+  {
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require("plugins.configs.lualine")
+		end,
+	},
 }
