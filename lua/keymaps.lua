87d4fd277c55da3b81e28f1c921c11eb6ee0ad20
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-08 08:12:41 +0700

    feat: add basic keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,13 @@
+local map = vim.keymap.set
+
+map("n", "<C-s>", "<cmd> w <CR>")
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jj", "<Esc>")
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd> w <CR><Esc>", { desc = "Save file" })
+map({ "n", "v" }, "B", "^", { desc = "Beginning of line" })
+map({ "n", "v" }, "W", "$", { desc = "End of line" })
+
+-- Coment
+map("n", "<leader>/", "gcc", { remap = true })
+map("v", "<leader>/", "gc", { remap = true })
