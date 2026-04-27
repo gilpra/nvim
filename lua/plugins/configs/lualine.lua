c5d35f74faa73bbbb57857ae20ed1f450983274b
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-27 21:56:44 +0700

    style: simplify component and section separators

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -3,8 +3,8 @@ return {
 		icons_enabled = true,
 		theme = "auto",
 
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "│", right = "│" },
+		section_separators = { left = "", right = "" },
 
 		disabled_filetypes = {
 			statusline = { "NvimTree", "starter" },
