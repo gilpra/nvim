bc3d6712843d5b25b2c4267ea25db1c4daf3ab2c
Gilang Pradana <gilangarya272001@gmail.com>
2026-05-21 08:13:44 +0700

    feat(term): add toggle terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -43,6 +43,60 @@ map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Pane: decrease height" })
 -- Terminal Mode
 map("t", "jk", [[<C-\><C-n>]], { desc = "Terminal: exit to normal mode" })
 
+local terminal_buf = nil
+local terminal_win = nil
+
+local function toggle_terminal()
+	-- Jika terminal sedang terbuka -> tutup split
+	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+		vim.api.nvim_win_close(terminal_win, true)
+		terminal_win = nil
+		return
+	end
+
+	-- Buat buffer baru jika belum ada
+	if not terminal_buf or not vim.api.nvim_buf_is_valid(terminal_buf) then
+		terminal_buf = vim.api.nvim_create_buf(false, true)
+		-- Pastikan buffer tidak ikut ke-wipe saat window-nya ditutup
+		vim.bo[terminal_buf].bufhidden = "hide"
+	end
+
+	-- Buat split
+	vim.cmd("botright split")
+	vim.api.nvim_win_set_height(0, 15)
+	terminal_win = vim.api.nvim_get_current_win()
+
+	-- Pasang buffer terminal ke window
+	vim.api.nvim_win_set_buf(terminal_win, terminal_buf)
+
+	-- Jalankan shell hanya jika buffer belum jadi terminal
+	if vim.bo[terminal_buf].buftype ~= "terminal" then
+		vim.fn.jobstart({ vim.o.shell }, { term = true })
+	end
+
+	-- Masuk terminal mode
+	vim.cmd("startinsert")
+end
+
+-- Otomatis tutup window jika shell exit
+vim.api.nvim_create_autocmd("TermClose", {
+	callback = function(args)
+		if args.buf == terminal_buf and terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+			vim.api.nvim_win_close(terminal_win, true)
+			terminal_win = nil
+			terminal_buf = nil
+		end
+	end,
+})
+
+map({ "n", "t" }, "<leader>t", toggle_terminal, {
+	desc = "Terminal: toggle",
+})
+
+map("t", "jk", [[<C-\><C-n>]], {
+	desc = "Terminal: exit to normal mode",
+})
+
 -- Tmux
 map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
