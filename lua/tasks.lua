f261c7caf591dacbaaee41168469cf0a68c6dfe7
Gilang Pradana <gilangarya272001@gmail.com>
2026-07-30 19:43:11 +0700

    refactor task.lua

--- lua/tasks.lua
+++ lua/tasks.lua
@@ -1,59 +1,40 @@
-local M = {}
-
 local tasks = {
 	c = {
 		compile = ":!gcc % -o %:r",
 		run = "./%:r",
 	},
-
-	python = {
-		run = "python %",
-	},
-
-	sh = {
-		run = "bash %",
-	},
-
-	html = {
-		run = "xdg-open %",
-	},
+	python = { run = "python %" },
+	sh = { run = "bash %" },
+	html = { run = "xdg-open %" },
 }
 
 local function expand_command(cmd)
-	local file = vim.fn.expand("%:t")
-	local file_root = vim.fn.expand("%:t:r")
+	local file = vim.fn.shellescape(vim.fn.expand("%:t"))
+	local root = vim.fn.shellescape(vim.fn.expand("%:t:r"))
 
-	return cmd
-		:gsub("%%:r", vim.fn.shellescape(file_root))
-		:gsub("%%", vim.fn.shellescape(file))
+	return cmd:gsub("%%:r", root):gsub("%%", file)
 end
 
 local function open_terminal(cmd)
-	local cwd = vim.fn.expand("%:p:h")
-
 	vim.cmd("botright split")
 	vim.cmd("resize 10")
 	vim.cmd("enew")
 
-	vim.fn.jobstart(
-		{ vim.o.shell, "-c", cmd },
-		{
-			term = true,
-			cwd = cwd,
-		}
-	)
+	vim.fn.jobstart({ vim.o.shell, "-c", cmd }, {
+		term = true,
+		cwd = vim.fn.expand("%:p:h"),
+	})
 
 	vim.cmd("startinsert")
 end
 
-local function run_task(kind)
-	local filetype = vim.bo.filetype
-	local task = tasks[filetype]
-	local cmd = task and task[kind]
+local function task(kind)
+	local ft = vim.bo.filetype
+	local cmd = tasks[ft] and tasks[ft][kind]
 
 	if not cmd then
 		vim.notify(
-			"No " .. kind .. " configured for filetype: " .. filetype,
+			("No %s configured for filetype: %s"):format(kind, ft),
 			vim.log.levels.WARN
 		)
 		return
@@ -61,26 +42,15 @@ local function run_task(kind)
 
 	if kind == "compile" then
 		vim.api.nvim_feedkeys(cmd, "n", false)
-		return
+	else
+		open_terminal(expand_command(cmd))
 	end
-
-	open_terminal(expand_command(cmd))
 end
 
-M.compile = function()
-	run_task("compile")
-end
+vim.keymap.set("n", "<leader>c", function()
+	task("compile")
+end, { desc = "Compile file" })
 
-M.run = function()
-	run_task("run")
-end
-
-vim.keymap.set("n", "<leader>c", M.compile, {
-	desc = "Compile file",
-})
-
-vim.keymap.set("n", "<leader>r", M.run, {
-	desc = "Run file",
-})
-
-return M
+vim.keymap.set("n", "<leader>r", function()
+	task("run")
+end, { desc = "Run file" })
