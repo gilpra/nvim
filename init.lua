bdf12a8af2ae7694bbcb6faf065f26f55e6598bf
Gilang Pradana <gilangarya272001@gmail.com>
2026-09-11 01:44:12 +0700

    refactor(vim-tmux-navigator): migrate to vim.pack

--- init.lua
+++ init.lua
@@ -34,6 +34,7 @@ vim.pack.add({
     { src = "https://github.com/williamboman/mason.nvim" },
     { src = "https://github.com/stevearc/conform.nvim" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = "https://github.com/christoomey/vim-tmux-navigator" },
 })
 
 -- Configure plugins
