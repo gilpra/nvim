d4ba2cf58c37d0c4bff3930a63806194435ba9f6
Gilang Pradana <gilangarya272001@gmail.com>
2026-09-11 10:56:09 +0700

    refactor: replace telescope with mini.pick

--- init.lua
+++ init.lua
@@ -6,28 +6,11 @@ require("vim._core.ui2").enable({})
 -- Local theme
 vim.cmd.colorscheme("monochrome")
 
--- Handle plugin build steps after installation or update
-vim.api.nvim_create_autocmd("PackChanged", {
-    callback = function(ev)
-        local name = ev.data.spec.name
-        local kind = ev.data.kind
-
-        if name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
-            vim.system({ "make" }, {
-                cwd = ev.data.path,
-            })
-        end
-    end,
-})
-
 -- Instal plugin with vim.pack
 vim.pack.add({
     { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter",         version = "main" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/nvim-telescope/telescope.nvim" },
-    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
-    { src = "https://github.com/saghen/blink.cmp",                        version = "v1" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    { src = "https://github.com/saghen/blink.cmp",                version = "v1" },
     { src = "https://github.com/rafamadriz/friendly-snippets" },
     { src = "https://github.com/L3MON4D3/LuaSnip" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
@@ -35,15 +18,15 @@ vim.pack.add({
     { src = "https://github.com/stevearc/conform.nvim" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/christoomey/vim-tmux-navigator" },
-    { src = "https://github.com/nvim-mini/mini.pairs",                    version = "stable" },
-    { src = "https://github.com/nvim-mini/mini.ai",                       version = "stable" },
-    { src = "https://github.com/nvim-mini/mini.surround",                 version = "stable" },
+    { src = "https://github.com/nvim-mini/mini.pairs",            version = "stable" },
+    { src = "https://github.com/nvim-mini/mini.ai",               version = "stable" },
+    { src = "https://github.com/nvim-mini/mini.surround",         version = "stable" },
+    { src = 'https://github.com/nvim-mini/mini.pick',             version = 'stable' },
 })
 
 -- Configure plugins
 require("plugins.oil")
 require("plugins.treesitter")
-require("plugins.telescope")
 require("plugins.blink")
 require("plugins.mason")
 require("plugins.conform")
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,44 @@
+local map = vim.keymap.set
+
 -- Auto pairs
-require('mini.pairs').setup()
+require("mini.pairs").setup()
 
--- Enchance a/i
-require('mini.ai').setup()
+-- Enhanced a/i
+require("mini.ai").setup()
 
--- Enchance surround
-require('mini.surround').setup()
+-- Enhanced surround
+require("mini.surround").setup()
+
+-- Picker
+require("mini.pick").setup({
+    options = {
+        use_cache = true,
+    },
+})
+
+
+-- ==================
+-- KEYMAPS
+-- ==================
+
+-- Find files
+map("n", "<leader>ff", function()
+    require("mini.pick").builtin.files({
+        tool = "rg",
+    })
+end, { desc = "Find files" })
+
+-- Recent files
+map("n", "<leader>fo", function()
+    require("mini.pick").start({
+        source = {
+            items = vim.v.oldfiles,
+            name = "Recent files",
+        },
+    })
+end, { desc = "Recent files" })
+
+-- Buffers
+map("n", "<leader>fb", function()
+    require("mini.pick").builtin.buffers()
+end, { desc = "Find buffers" })
